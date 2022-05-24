438309e20d1f7f8ceace989623252f0997b3fd09
ramamike <7629756@gmail.com>
2022-05-24 16:08:02 +0300

    create new account, @GetMapping and draft html

--- src/main/java/com/blockwit/booking/controllers/SecurityController.java
+++ src/main/java/com/blockwit/booking/controllers/SecurityController.java
@@ -1,13 +1,25 @@
 package com.blockwit.booking.controllers;
 
+import com.blockwit.booking.model.NewAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
+@RequestMapping("/app")
 public class SecurityController {
 
-    @GetMapping("/app/login")
+    @GetMapping("/login")
     public String home() {
         return "front/login";
     }
+
+    @GetMapping("/account/new")
+    public ModelAndView createAccountGet() {
+        return new ModelAndView("front/account-new", Map.of("newAccount", new NewAccount()));
+    }
+
 }
---
+++ src/main/java/com/blockwit/booking/model/NewAccount.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2017-present BlockWit, LLC. or BlockWit Team All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.blockwit.booking.model;
+
+import com.blockwit.booking.controllers.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewAccount {
+
+  @NotNull
+  @Size(min = 3, max = 50)
+  @Pattern(regexp = Constants.REGEXP_LOGIN, message = "{model.newaccount.login.regexp.error}")
+  private String login;
+
+//  @NotNull
+//  @Email(message = "{model.newaccount.email.regexp.error}")
+//  private String email;
+//
+//  @NotNull
+//  @Pattern(regexp = Constants.REGEXP_PASSWORD, message = "{model.newaccount.pwd.regexp.error}")
+//  private String password;
+//
+//  @NotNull
+//  @Pattern(regexp = Constants.REGEXP_PASSWORD, message = "{model.newaccount.repwd.regexp.error}")
+//  private String repassword;
+
+}
