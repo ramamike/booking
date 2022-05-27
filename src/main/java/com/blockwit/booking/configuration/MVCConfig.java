59123a9b1fe0fed10339957cd5d1a7bb98d1c8a2
ramamike <7629756@gmail.com>
2022-05-27 15:15:49 +0300

    MvcConfiguration - two languages

--- src/main/java/com/blockwit/booking/configuration/MVCConfig.java
+++ src/main/java/com/blockwit/booking/configuration/MVCConfig.java
@@ -13,15 +13,20 @@
  */
 package com.blockwit.booking.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.http.CacheControl;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
@@ -48,6 +53,11 @@ public class MVCConfig implements WebMvcConfigurer {
 			"/hotels/edit/**",
 			"/hotels/book/**"
 		);
+
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		registry.addInterceptor(localeChangeInterceptor);
+
 	}
 
 	@Override
@@ -55,6 +65,14 @@ public class MVCConfig implements WebMvcConfigurer {
 		PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
 		resolver.setOneIndexedParameters(true);
 		argumentResolvers.add(resolver);
+	}
+
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+		return cookieLocaleResolver;
 	}
 
 }
